2e691797019160828f253214fde7880d25b3338e
Dragon-Slayer777 <53530816+Dragon-Slayer777@users.noreply.github.com>
2020-11-05 22:15:12 +0530

    Update death.py

--- death.py
+++ death.py
@@ -2,7 +2,6 @@ import math
 import random
 
 def death_val(x):
-  if x > 5:
   
-    return (0.05*(math.tanh((x - (67 - random.random()*20)/27 - random.random()*10)) + 0.05 + (0.00002*(50 - random.random()*10 - x))
+   return (0.05*(math.tanh((x - (67 - random.random()*20)/27 - random.random()*10)) + 0.05 + (0.0002*(50 - random.random()*10 - x))
     
